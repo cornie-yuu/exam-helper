0a806ff9f00b6cbef7d6ffd23f3a8cab58cc193c
cornie-yuu <cornie-yuu@users.noreply.github.com>
2026-07-29 11:43:54 +0800

    fix: change base to / for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/exam-helper/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
